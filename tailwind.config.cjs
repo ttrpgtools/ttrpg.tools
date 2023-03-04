ed0c0f6218cedd3e3db0ff646e74f1a8ef71a364
Colin Bate <colin@colinbate.com>
2023-03-04 01:28:30 -0400

    Initial placeholder landing page.

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        discord: '#5865f2',
+        patreon: '#F1465A'
+      }
+    },
+  },
+  plugins: [],
+}
