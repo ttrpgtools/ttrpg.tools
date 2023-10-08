4ae808642c3998f4b241b8592f7c59fc4f260e3c
Colin Bate <colin@colinbate.com>
2023-10-07 23:01:55 -0300

    Add article and view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-//import toml from './toml-content';
-
-// import remarkFrontmatter from 'remark-frontmatter';
-//import remarkTomlFrontmatter from './remark-astro-toml-frontmatter';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), svelte(), tailwind()],
-  markdown: {
-    // remarkPlugins: [
-    //   [remarkFrontmatter, 'toml'],
-    //   remarkTomlFrontmatter,
-    // ],
-  },
-  experimental: {
-    assets: true
-  },
 });
