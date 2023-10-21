7ad7bb1fb0af8bcf204810fa90cb7349d20db8b6
Colin Bate <colin@colinbate.com>
2023-10-21 00:10:43 -0300

    Update Patreon logo

--- src/lib/partials/Icon.svelte
+++ src/lib/partials/Icon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   export let icon: string | undefined;
   export let size: string = '1rem';
-  $: href = `/icons.svg#${icon}`;
+  $: href = `/icons.svg?1#${icon}`;
 </script>
 {#if icon}
 <svg preserveAspectRatio="xMidYMid meet" class="inline-block m-0 !mt-0 leading-none" style:height={size} style:width={size}><use xlink:href={href}/></svg>
